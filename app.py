a3879ebee447cc4ff16511513368cd2adc29f0ea
glorytexer7 <glorytexer7@gmail.com>
2025-11-05 15:52:50 +0330

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # اضافه برای رفع مشکل Cross-Origin
 
 app = Flask(__name__)
+CORS(app)  # فعال کردن CORS برای همه مسیرها
 
 @app.route('/')
 def home():
